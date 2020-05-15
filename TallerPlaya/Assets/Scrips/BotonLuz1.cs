ffb85e959720f39639e1e42cc99f6a93c1685cb1
intinazca <60673220+intinazca@users.noreply.github.com>
2020-05-14 20:05:32 -0500

    talleerplaya
    pana es el de la playa pero se carga con ese nombre

---
+++ TallerPlaya/Assets/Scrips/BotonLuz1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+
+public class BotonLuz1 : MonoBehaviour
+{
+    public GameObject DirectionalLight;
+    int estado =0;
+
+    void Start()
+    {
+        DirectionalLight.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+    //DirectionalLight.SetActive(false);
+
+   public void LuzEscenario()
+      {
+        if (estado == 0)
+        {
+            DirectionalLight.SetActive(true);
+            estado = 1;
+        }
+
+        else if (estado == 1)
+        {
+            DirectionalLight.SetActive(false);
+            estado = 0;
+        }
+    }
+
+}
