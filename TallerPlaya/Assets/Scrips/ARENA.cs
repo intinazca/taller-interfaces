27e25b821adbaa4e218e8ab84cfc0e614ced11a6
Juan Pablo Duque Arce <juanpablo.multimedia@gmail.com>
2020-05-15 17:05:19 -0500

    se añade Nieve y Tormenta de arena
    Se añade particula de nieve y tormenta de arenas con su respectiva script y efecto de sonido al escenario del lago

---
+++ TallerPlaya/Assets/Scrips/ARENA.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARENA : MonoBehaviour
+{
+    public GameObject arena;
+    int estado = 0;
+
+    void Start()
+    {
+        arena.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+    //DirectionalLight.SetActive(false);
+
+    public void ArenaEscenario()
+    {
+        if (estado == 0)
+        {
+            arena.SetActive(true);
+            estado = 1;
+        }
+
+        else if (estado == 1)
+        {
+            arena.SetActive(false);
+            estado = 0;
+        }
+    }
+}
---
+++ TallerPlaya/Assets/Scrips/BotonNieve.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonNieve : MonoBehaviour
+{
+    public GameObject nieve;
+    int estado = 0;
+
+    void Start()
+    {
+        nieve.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+    //DirectionalLight.SetActive(false);
+
+    public void NeveEscenario()
+    {
+        if (estado == 0)
+        {
+            nieve.SetActive(true);
+            estado = 1;
+        }
+
+        else if (estado == 1)
+        {
+            nieve.SetActive(false);
+            estado = 0;
+        }
+    }
+}
