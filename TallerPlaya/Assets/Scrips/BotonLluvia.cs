04190aac9ef25007880fbdde3458af935cfe2701
Juan Pablo Duque Arce <juanpablo.multimedia@gmail.com>
2020-05-15 01:49:29 -0500

    Se le agrega Fuego y lluvia
    Se le agrega particulas de fuego y lluvia(con su respectivo boton) con efecto de sonido y un pointlight en el caso del fuego

---
+++ TallerPlaya/Assets/Scrips/BotonLluvia.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonLluvia : MonoBehaviour
+{
+    public GameObject StormRain;
+    int estado = 0;
+
+    void Start()
+    {
+        StormRain.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+    //DirectionalLight.SetActive(false);
+
+    public void LluviaEscenario()
+    {
+        if (estado == 0)
+        {
+            StormRain.SetActive(true);
+            
+
+            estado = 1;
+            print("Lluvia on");
+        }
+
+        else if (estado == 1)
+        {
+            StormRain.SetActive(false);
+            
+            print("Lluvia off");
+            estado = 0;
+
+        }
+    }
+
+}
---
+++ TallerPlaya/Assets/Scrips/Fuego.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuego : MonoBehaviour
+{
+    public GameObject fuego;
+    int estado = 0;
+
+    void Start()
+    {
+        fuego.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+    //DirectionalLight.SetActive(false);
+
+    public void FuegoEscenario()
+    {
+        if (estado == 0)
+        {
+            fuego.SetActive(false);
+            estado = 1;
+        }
+
+        else if (estado == 1)
+        {
+            fuego.SetActive(true);
+            estado = 0;
+        }
+    }
+}
